e900c376ab08bb0f12a09688fb315be75f9cc430
Zchappie <zeek.uni@outlook.com>
2020-02-15 10:50:35 +0100

    Create arr_removeDuplicates.py

---
+++ leetcode-easy-collection/python3/arr_removeDuplicates.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+        if len(nums) == i:
+            return i
+        while i < len(nums):
+            if nums[i] in nums[:i]:
+                del nums[i]
+            else:
+                i += 1
+        return i
+
+    """
+    Use the property of set, to get unique items 
+    """
+    # def removeDuplicates(self, nums: List[int]) -> int:
+    #     nums[:] = sorted(set(nums))
+    #     return len(nums)
