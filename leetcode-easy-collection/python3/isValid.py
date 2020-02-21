39858264977066509b42004e9ef6056837bd431e
Zchappie <zeek.uni@outlook.com>
2020-02-21 07:18:34 +0100

    add better solution

--- leetcode-easy-collection/python3/isValid.py
+++ leetcode-easy-collection/python3/isValid.py
@@ -25,7 +25,22 @@ class Solution:
                 s = s[:i] + s[i+2:]
                 return self.isValid(s)
 
-            
+        """
+        Using a stack can solve this problem easily.
+        """
+    	# def isValid(self, s):
+        # d = {'(':')', '{':'}','[':']'}
+        # stack = []
+        # for i in s:
+        #     if i in d:  # 1
+        #         stack.append(i)
+        #     elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+        #         return False
+        # return len(stack) == 0 # 3
+	
+        # 1. if it's the left bracket then we append it to the stack
+        # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+        # 3. finally check if the stack still contains unmatched left bracket
                     
 solu = Solution()
 print(solu.isValid("((()))}"))
