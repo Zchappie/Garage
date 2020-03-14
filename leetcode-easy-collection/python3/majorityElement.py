4a16174564a30a821d7338cbd014e34cb9ee4965
Zchappie <zeek.uni@outlook.com>
2020-03-14 08:26:20 +0100

    even though, there is a smarter solution

--- leetcode-easy-collection/python3/majorityElement.py
+++ leetcode-easy-collection/python3/majorityElement.py
@@ -4,4 +4,13 @@ class Solution:
         for i in elementSet:
             if nums.count(i) > int(len(nums)/2):
                 return i
-                
+
+    def majorityElement(self, nums: List[int]) -> int:
+        """
+        More simple one, if one integer has more than half in number,
+        then the middle point of the sorted list should be this element.
+
+        This only increase the speed, but not the memory.
+        """
+        nums.sort()
+        return nums[int(len(nums)/2)]
