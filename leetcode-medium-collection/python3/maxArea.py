0035ae937f4bea3eb9cf3329e5baf16df8944b71
Zchappie <zeek.uni@outlook.com>
2020-04-15 09:17:28 +0200

    Update maxArea.py

--- leetcode-medium-collection/python3/maxArea.py
+++ leetcode-medium-collection/python3/maxArea.py
@@ -1,4 +1,20 @@
 from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        
+        up = len(height) - 1
+        low = 0
+        maxarea = 0
+        for i in range(len(height)):
+            if height[low] < height[up]:
+                area = abs(up - low) * height[low]
+                low += 1
+            
+            else:
+                area = abs(up - low) * height[up]
+                up -= 1
+            
+            if area > maxarea:
+                maxarea = area
+                
+        return maxarea
+                
