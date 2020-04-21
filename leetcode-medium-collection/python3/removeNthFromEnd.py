f1197fe93b25c8af5a96cec30bd78875c17d0914
Zchappie <zeek.uni@outlook.com>
2020-04-21 08:58:04 +0200

    add an ac solu

--- leetcode-medium-collection/python3/removeNthFromEnd.py
+++ leetcode-medium-collection/python3/removeNthFromEnd.py
@@ -24,3 +24,16 @@ class Solution:
         except:
             curNode.next = None
             return newList
+
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        def index(node):
+            if not node:
+                return 0
+            i = index(node.next) + 1
+            if i > n:
+                node.next.val = node.val
+            return i
+        index(head)
+        return head.next
