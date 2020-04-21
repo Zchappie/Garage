c41664b198274b2272f9af63d592dc35973aa776
Zchappie <zeek.uni@outlook.com>
2020-04-21 08:56:44 +0200

    I think I really need to learn what is linked list

--- leetcode-medium-collection/python3/removeNthFromEnd.py
+++ leetcode-medium-collection/python3/removeNthFromEnd.py
@@ -6,16 +6,21 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # consider the deleted node is at the beginning
         fast, slow = 0, 0
         fastNode = head
         newList = head
-        newNode = head
+        curNode = newList.next
         while fastNode.next:
             fastNode = fastNode.next
             fast += 1
             if fast >= n + 1:
                 slow += 1
-
+                curNode = curNode.next
 
         # while loop stops at slow reach the need-to-be-deleted node
-        if 
+        try:
+            curNode = curNode.next.next
+        except:
+            curNode.next = None
+            return newList
