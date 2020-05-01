212cf1393dbc7fd5e8193484f7b3028565be7927
Zchappie <zeek.uni@outlook.com>
2020-05-01 06:50:41 +0200

    Update search.py

--- leetcode-medium-collection/python3/search.py
+++ leetcode-medium-collection/python3/search.py
@@ -5,3 +5,30 @@ class Solution:
             return nums.index(target)
         except ValueError:
             return -1
+
+    """
+    Search the starting point.
+    """
+    def search2(self, nums, target):
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) / 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
