ea20ba81ae2b7d018cc6069160907c4d1f382857
Zchappie <zeek.uni@outlook.com>
2020-05-20 11:11:58 +0200

    Update convertRangeImg.py

--- ImgProcessingPythonUtils/convertRangeImg.py
+++ ImgProcessingPythonUtils/convertRangeImg.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import os
 import shutil
+import csv
 
 def moveFilesToAnotherFolderAccordingToName(sourceFolder, targetFolder, targetChar):
     """
@@ -21,6 +22,7 @@ def normalizeRangeFiles(sourceFolder, targetFolder):
     targetFolder: target folder to move the files to, after the file is processed
     """
     counter = 0
+    counterZero = 0
     for fileName in os.listdir(sourceFolder):
         counter += 1        
         filePath = os.path.join(sourceFolder, fileName)
@@ -30,8 +32,9 @@ def normalizeRangeFiles(sourceFolder, targetFolder):
         maxPxlValue = rangeImg.max()
         minPxlValue = rangeImg.min()
         if maxPxlValue == minPxlValue:
-            print("Dividing zero here at ", counter, "-th image, and it has been moved to a trash, your majesty.")
-            shutil.move(filePath, './ranges_without_process/')
+            counterZero += 1
+            print("Dividing zero here at ", counter, "-th image, we ignored it, your majesty.")
+            # shutil.move(filePath, './ranges_without_process/')
             continue
         alpha = 255/(maxPxlValue-minPxlValue)
         
@@ -40,7 +43,8 @@ def normalizeRangeFiles(sourceFolder, targetFolder):
         normalizedImg = np.float32(rangeImg) * alpha + beta
         normalizedImg = np.uint8(normalizedImg)
         im_color = cv2.applyColorMap(normalizedImg, cv2.COLORMAP_BONE)
-        cv2.imwrite(filePath, im_color)
+        cv2.imwrite(os.path.join(targetFolder, fileName), im_color)
+    print("In total, we have ", counterZero, " files which are purely black, your majesty.")
 
 def moveColorImgsToAnotherFolder(sourceFolder, targetFolder):
     for fileName in os.listdir(sourceFolder):        
@@ -63,18 +67,45 @@ def findPossibleValidRangeFiles(sourceFolder, targetFolder):
         # np.reshape(img, -1)
 
 
+# dig out more information from small ranges
+def countSmallRangeFilesInfo(sourceFolder):
+    f = open('/home/mengl/Downloads/small_ranges_info.csv', 'w')
+
+    with f:
+        writer = csv.writer(f)
+        writer.writerow(['fileName', 'firstDim', 'secondDim', 'thirdDim', 'minPxlValue', 'maxPxlValue'])
+        # rows = []
+
+        for fileName in os.listdir(sourceFolder):   
+            row = [fileName, 'nan', 'nan', 'nan', 'nan', 'nan']     
+            filePath = os.path.join(sourceFolder, fileName)
+            img = cv2.imread(filePath)
+            try:
+                row[1], row[2], row[3] = img.shape
+                (row[4], row[5], _, _) = cv2.minMaxLoc(img)
+            except:
+                pass
+            writer.writerow(row.copy())
+
+        # for row in nms:
+        #     writer.writerow(row)
+
+
 # sourceFolder = './nlr_imgs_small/'
 # targetFolder = './ranges/'
 # moveFilesToAnotherFolderAccordingToName(sourceFolder, targetFolder, 'range')
 
-# sourceFolder = './ranges/'
-# targetFolder = './ranges/'
+# sourceFolder = './small_files/'
+# targetFolder = './small_files/'
 # normalizeRangeFiles(sourceFolder, targetFolder)
 
 # sourceFolder = './ranges/'
 # targetFolder = './processed/'
 # moveColorImgsToAnotherFolder(sourceFolder, targetFolder)
 
-sourceFolder = './processed/'
-targetFolder = './rectangleImgs/'
-findPossibleValidRangeFiles(sourceFolder, targetFolder)
+# sourceFolder = './processed/'
+# targetFolder = './rectangleImgs/'
+# findPossibleValidRangeFiles(sourceFolder, targetFolder)
+
+sourceFolder = './nlr_small_range/'
+countSmallRangeFilesInfo(sourceFolder)
